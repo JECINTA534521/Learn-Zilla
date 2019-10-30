2ba73c61489607aae655b13d19daadcf3db89dff
jecinta <jecintawanjirug@gmail.com>
2019-10-30 16:27:42 +0300

    this creates a class registration form to store the registraton details

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -2,3 +2,13 @@ from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,BooleanField,SubmitField
 from wtforms.validators import Required,Email,EqualTo
 from wtforms import ValidationError
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Your Email Address',validators=[Required(),Email()])
+    username = StringField('Enter your username',validators = [Required()])
+    firstname = StringField('Enter your first name',validators = [Required()])
+    lastname = StringField('Enter your last name',validators = [Required()])
+    password = PasswordField('Password',validators = [Required(), EqualTo('password_confirm',message = 'Passwords must match')])
+    password_confirm = PasswordField('Confirm Passwords',validators = [Required()])
+    submit = SubmitField('Sign Up')
+
