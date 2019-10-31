cc4b02ff027006e87273a28d526ef4c46dd161f4
jecinta <jecintawanjirug@gmail.com>
2019-10-31 15:09:00 +0300

    this comments out the class role

--- app/models.py
+++ app/models.py
@@ -69,12 +69,12 @@ class Review(db.Model):
         reviews = Review.query.filter_by(book_id = id).all()
         return reviews
 
-class Role(db.Model):
-    __tablename__ = "roles"
+# class Role(db.Model):
+#     __tablename__ = "roles"
 
-    id = db.Column(db.Integer,primary_key = True)
-    name = db.Column(db.String(255))
-    users = db.relationship("User", backref = "role", lazy = "dynamic")
+#     id = db.Column(db.Integer,primary_key = True)
+#     name = db.Column(db.String(255))
+#     users = db.relationship("User", backref = "role", lazy = "dynamic")
 
-    def __repr__(self):
-        return f"User {self.name}"
+#     def __repr__(self):
+#         return f"User {self.name}"
